3a03ac5f987d3b4a9e27531d19d1431c0518a931
Hayley Payne <hcookiepayne@gmail.com>
2020-07-30 16:00:07 -0400

    Reworking Java

---
+++ carousel.js
@@ -0,0 +1,9 @@
+//Had to redo my java, definitely did not understand the original
+//most of these comments are for me to better understand what I'm doing with java lol
+
+//NOTES
+//variables are used to store data values using the keyword var
+//creating a variable is called declaring a variable
+//a function performs a task and must be called in order to execute
+//if statements executes code when a condition is met
+
